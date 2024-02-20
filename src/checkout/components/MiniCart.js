8c3727887ddecb387af1b621df3fa1471ebdaeb6
Fabricius Seifert <fabricius.seifert@neuland-bfi.de>
2024-02-20 09:51:00 +0100

    mini cart styles

--- src/checkout/components/MiniCart.js
+++ src/checkout/components/MiniCart.js
@@ -5,7 +5,9 @@ export default ({ req }) => {
   const lineItems = readFromCookie(req);
   const quantity = lineItems.reduce((t, { quantity }) => t + quantity, 0);
   return html`<div class="c_MiniCart" data-boundary="checkout-minicart">
-    <a href="/checkout/cart" class="c_MiniCart__icon"> 🛒 ${quantity} </a>
+    <a href="/checkout/cart" class="c_MiniCart__icon" ontouchstart>
+      <div class="c_MiniCart__icon__content">🛒 ${quantity}</div>
+    </a>
     <a href="/checkout/cart" class="c_MiniCart__button">go to cart</a>
   </div>`;
 };
