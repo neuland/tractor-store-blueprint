e2d2d4d6fe0a0deac13c5c9ad8eec2e08d89c448
Fabricius Seifert <fabricius.seifert@neuland-bfi.de>
2024-03-08 11:39:05 +0100

    nicer blobs

--- src/decide/components/Variant.js
+++ src/decide/components/Variant.js
@@ -1,7 +1,7 @@
 import { html } from "../utils.js";
 
 export default ({ sku, name, selected, color }) => {
-  return html`<li>
+  return html`<li class="d_Variant">
     <i class="d_Variant__color" style="--background-color: ${color}"></i>
     ${selected
       ? html`<strong>${name}</strong>`
