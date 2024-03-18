9ccf2e1de1fefb0673db829cee8a118df1bf08c9
Michael Geers <michael@geers.tv>
2024-03-18 16:13:22 +0100

    thanks

--- src/checkout/pages/Thanks.js
+++ src/checkout/pages/Thanks.js
@@ -8,8 +8,8 @@ export default ({ c }) => {
   const content = html`
     ${Header({ c })}
     <main class="c_Thanks">
-      <h2>Thanks for your order!</h2>
-      <p>We'll notify you, when its ready for pickup.</p>
+      <h2 class="c_Thanks__title">Thanks for your order!</h2>
+      <p class="c_Thanks__text">We'll notify you, when its ready for pickup.</p>
 
       ${Button({
         href: "/",
