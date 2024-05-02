7547c9eabd0120245f7eefc17e0895922a4f9ce6
Fabricius Seifert <fabricius.seifert@neuland-bfi.de>
2024-05-02 10:40:36 +0200

    price alignment small

--- src/checkout/components/LineItem.js
+++ src/checkout/components/LineItem.js
@@ -23,31 +23,34 @@ export default ({ sku, id, name, quantity, total, image }) => {
       <a href="${url}" class="c_LineItem__name">
         <strong>${name}</strong><br />${sku}
       </a>
-      <div class="c_LineItem__quantity">${quantity}</div>
 
-      <form action="/checkout/cart/remove" method="post">
-        <input type="hidden" name="sku" value="${sku}" />
-        ${Button({
-          variant: "secondary",
-          rounded: true,
-          type: "submit",
-          value: "remove",
-          size: "small",
-          title: `Remove ${name} from cart`,
-          children: html`<svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            height="20"
-            width="20"
-            viewBox="0 0 48 48"
-          >
-            <path
-              fill="#000"
-              d="m40 5.172-16 16-16-16L5.171 8l16.001 16L5.171 40 8 42.828l16-16 16 16L42.828 40l-16-16 16-16L40 5.172Z"
-            />
-          </svg>`,
-        })}
-      </form>
+      <div class="c_LineItem__quantity">
+        <span>${quantity}</span>
+
+        <form action="/checkout/cart/remove" method="post">
+          <input type="hidden" name="sku" value="${sku}" />
+          ${Button({
+            variant: "secondary",
+            rounded: true,
+            type: "submit",
+            value: "remove",
+            size: "small",
+            title: `Remove ${name} from cart`,
+            children: html`<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              height="20"
+              width="20"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#000"
+                d="m40 5.172-16 16-16-16L5.171 8l16.001 16L5.171 40 8 42.828l16-16 16 16L42.828 40l-16-16 16-16L40 5.172Z"
+              />
+            </svg>`,
+          })}
+        </form>
+      </div>
       <div class="c_LineItem__price">${total} Ø</div>
     </div>
   </li>`;
