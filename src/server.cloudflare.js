715e9cb241d5e58bc783644e2338b4966ef9fcda
Michael Geers <michael@geers.tv>
2024-06-11 15:23:25 +0200

    cache & cors header

--- src/server.cloudflare.js
+++ src/server.cloudflare.js
@@ -4,8 +4,18 @@ import manifest from "__STATIC_CONTENT_MANIFEST";
 
 const app = createServer();
 
+const setCacheAndCors = (req, res, next) => {
+  res.setHeader("Cache-Control", "public, max-age=3600"); // 1 hour cache
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+};
+
+app.use("/cdn/*", setCacheAndCors);
 app.use("/cdn/*", serveStatic({ root: "./", manifest }));
 ["explore", "decide", "checkout"].forEach((team) => {
+  app.use(`/${team}/static/*`, setCacheAndCors);
   app.use(`/${team}/static/*`, serveStatic({ root: `./`, manifest }));
 });
 
